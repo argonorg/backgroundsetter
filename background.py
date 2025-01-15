25db831685c6713540bc1fb1e8477985731ba2ef
eSKa-0 <166024351+eSKa-0@users.noreply.github.com>
2025-01-15 18:00:31 +0100

    Add files via upload

---
+++ background.py
@@ -0,0 +1,30 @@
+import os
+import ctypes
+import urllib.request
+
+def download_image(url, save_path):
+    try:
+        urllib.request.urlretrieve(url, save_path)
+        print(f"Desktop background downloaded to: {save_path}")
+    except Exception as e:
+        print(f"Failed to download desktop background: {e}")
+
+def set_wallpaper(image_path):
+    try:
+        result = ctypes.windll.user32.SystemParametersInfoW(20, 0, image_path, 3)
+        if result:
+            print("Task completed.")
+        else:
+            print("Task failed.")
+    except Exception as e:
+        print(f"Error setting background: {e}")
+
+def main():
+    url = "https://raw.githubusercontent.com/argonorg/backgroundsetter/refs/heads/main/RadonBackground.png?token=GHSAT0AAAAAAC5MGPPTGWTIDEVZAH7VBJ5CZ4H5D4A"
+    pictures_folder = os.path.join(os.environ['USERPROFILE'], "Pictures")
+    save_path = os.path.join(pictures_folder, "DesktopBackground.png")
+    download_image(url, save_path)
+    set_wallpaper(save_path)
+
+if __name__ == "__main__":
+    main()
